5a291a8dba2d6598861af5ee9280303f4c065c4f
haeun <gkdms1120s@gmail.com>
2021-08-05 12:27:31 +0900

    order entity added

---
+++ src/main/java/springstudy/spring/domain/OrderItem.java
@@ -0,0 +1,31 @@
+package springstudy.spring.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="order_item")
+@Getter @Setter
+public class OrderItem {
+    @Id
+    @GeneratedValue
+    @Column(name = "order_product_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @Column(name = "item_option")
+    private String option;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @Column(name="item_count")
+    private int count;
+
+}
