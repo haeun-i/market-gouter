553c53bb79c450dac7b77af5c0295644d227d6f5
yoosung-kim <helios1127@naver.com>
2021-08-14 14:57:48 +0900

    photo

--- src/main/java/springstudy/spring/File/Photo.java
+++ src/main/java/springstudy/spring/File/Photo.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 @Getter
 @Entity
 @Table(name = "file")
-@NoArgsConstructor (access = AccessLevel.PUBLIC)
+@NoArgsConstructor
 // noargsconstructor - 파라미터가 없는 생성자를 생성해준다.
 public class Photo {
 }
