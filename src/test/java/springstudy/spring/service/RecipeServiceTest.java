7f1f363db4bc3263aea34efcedad57703b3fbec9
yoosung-kim <helios1127@naver.com>
2021-08-21 13:49:35 +0900

    0821

---
+++ src/test/java/springstudy/spring/service/RecipeServiceTest.java
@@ -0,0 +1,18 @@
+package springstudy.spring.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class RecipeServiceTest {
+
+    @Test
+    public void 레시피추가() throws Exception{
+
+    }
+
+}
